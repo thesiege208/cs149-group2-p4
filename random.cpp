3e7170983b68077e9b6afa413fb25592fac12b62
Tianyun <451617222@qq.com>
2018-10-30 03:31:46 +0800

    fixed: first final draft

--- random.cpp
+++ random.cpp
@@ -1,79 +1,107 @@
 #include <iostream>
 #include <map>
 #include <list>
-#include<fstream>
+#include <fstream>
+#include <stdio.h>
 #include <unistd.h>
-
+#include <iomanip>
+#include <sys/time.h>
 
 using namespace std;
 
 ofstream ofile;
 
-typedef struct pag  {
-    int    PageNumber;
-    string BindProcessName;
-}Page, *pPage;
+typedef struct Page {
+    int pageNumber;
+    string bindProcessName;
+} page;
 
-struct Prcoess  {
-    string        Name;
-    int           Size;
-    int           ArrivalTime;
-    int           ServiceTime;
-    map<int,Page> PageList;
-    int           HitCounts;
-    int           MissingCount;
-};
-
-
-void Reset(Page *page) {
-
-    ofile.open("/home/page.log");
-    ofile << "Process " << page->BindProcessName << ", page " <<
-          page->PageNumber << " evicted." << endl;
-    ofile.close();
-
-    //LOG输出
-    cout << "Process " << page->BindProcessName << ", page " <<
-         page->PageNumber << " evicted." << endl;
-
-    page->BindProcessName = "";
-    page->PageNumber = 0;
-}
-
-bool IsUsed(Page *page) {
-    string str = "";
-    return str.compare(page->BindProcessName) == 0 ? true : false;
-}
-
-void  *GetPage(Page *page){
-    return NULL;
-}
-
-void initVariable(){
-
-}
-
+typedef struct Prcoess {
+    string name;      //随便取
+    int size;         //5, 11, 17, or 31 MB
+    int arrivalTime;  //小于6000的随机
+    int serviceTime;  //1, 2, 3, 4, or 5 seconds.
+    map<int, page> pageList;     //长度 跟Size一毛一样哟 类型page
+    int hitCounts;    //start with 0
+    int missingCount;
+} process;
 
 int TotalTime = 6000;
 int CurrentTime = 0;
-list<Page>    memoryFreePage(100); //size 100 类型Page
-list<Prcoess> JobList(150);     //size 150, 类型Process
+list<page> memoryFreePage(100);    //size 100  类型Page
+list<Prcoess> JobList(150);        //size 150, 类型Process
+int arrSize[4] = {5, 11, 17, 31};  //random for 5, 11, 17, or 31 MB
 
-void AssignPage(Prcoess *prcoess, int number){
-//    auto page = GetPage(prcoess->PageList);
+int64_t getCurrentTime() {
+    struct timeval time;
+    gettimeofday(&time, NULL);
+    return time.tv_sec;
+}
 
-//    if IsUsed(){
-//        Reset(page)
-//    }
-//    page.BindProcessName = process.Name
-//    page.PageNumber = number
-//    process.PageList[number]=page
-//    /*
-//     file << setfill('0') << setw(2) << timestamp / 1000
-//             << "s: Process " << curr.name << " referenced page " << i
-//             << ". Page not in memory. ";
+void Reset(page *page) {
+    ofile.open("page.txt");
+    ofile << "Process " << page->bindProcessName << ", page " <<
+          page->pageNumber << " evicted."<<endl;
+    ofile.close();
+    cout << "Process " << page->bindProcessName << ", page " <<
+         page->pageNumber << " evicted." << endl;
 
-    prcoess->MissingCount++;
+    page->bindProcessName = "";
+    page->pageNumber = 0;
+}
+
+bool IsUsed(const string name) {
+    string str = "";
+    return str.compare(name) == 0 ? true : false;
+}
+
+page *GetPage(page pa) {
+    pa = memoryFreePage.front();
+    memoryFreePage.pop_front();
+    return &pa;
+}
+
+bool sortArrTime(const process &m1, const process &m2) {
+    return m1.arrivalTime < m2.arrivalTime;
+}
+
+void initVariable() {
+    for (int i = 0; i < 150; ++i) {
+        process proInit;
+        int size = (rand() % 4);
+        proInit.size = arrSize[size];
+        proInit.arrivalTime = (rand() % 6000) + 1;
+        proInit.serviceTime = (rand() % 5) + 1;
+        char s[40];
+        sprintf(s, "%s%d", "P", proInit.arrivalTime);
+        proInit.name = s;
+
+        JobList.push_back(proInit);
+    }
+    JobList.sort(sortArrTime);
+    for (int j = 0; j < 100; ++j) {
+        page pagInit;
+        pagInit.pageNumber = 0;
+        pagInit.bindProcessName = "";
+        memoryFreePage.push_back(pagInit);
+    }
+}
+
+void AssignPage(process *prc, int number) {
+    auto pag = GetPage(prc->pageList[number]);
+    if (IsUsed(pag->bindProcessName)) {
+        Reset(pag);
+    }
+    pag->bindProcessName = prc->name;
+    pag->pageNumber = number;
+    prc->pageList[number] = *pag;
+
+    ofile.open("AssignPage.txt");
+    ofile << setfill('0') << setw(2) << getCurrentTime() / 1000
+          << "s: Process " << prc->name << " referenced page " << prc->pageList[number].pageNumber
+          << ". Page not in memory. ";
+    ofile.close();
+    prc->missingCount++;
 }
 
 int locality(int i, int totalPages) {
@@ -89,74 +117,80 @@ int locality(int i, int totalPages) {
     return j;
 }
 
-void RandomRepalce(Prcoess *process, int pageNumber){
+void RandomRepalce(process *prc, int pageNumber) {
+    int oldKey = rand() % 150 + 1;
+    page *page;
 
+    page->pageNumber = pageNumber;
+    prc->pageList[pageNumber] = *page;
 
-    int  oldKey;
-    Page *page;
-
-//    //随机获取一个Key
-//    for oldKey,page=range process->{
-//        break
-//    }
-//    page.PageNumber = pageNumber
-//    delete(process.PageList,oldKey)
-//    process.PageList[pageNumber]=page
-    /*
-    file << setfill('0') << setw(2) << timestamp / 1000
-                         << "s: Process " << curr.name << " referenced page " << pageNumber
-                         << ". Page not in memory. Process " << procNum << ", page "
-                         << oldKey << " evicted." << endl;
-    */
+    ofile.open("RandomRepalce.txt");
+    ofile << setfill('0') << setw(2) << getCurrentTime() / 1000
+          << "s: Process " << prc->name << " referenced page " << pageNumber
+          << ". Page not in memory. Process " << prc->pageList[pageNumber].pageNumber << ", page "
+          << oldKey << " evicted.";
+    ofile.close();
 }
 
 void Run() {
-    while (JobList.size() != 0 && TotalTime <= CurrentTime) {
-
+    CurrentTime = getCurrentTime();
+    while (JobList.size() != 0) {
         int currentReferenceNumber = 0;
         Prcoess job = JobList.front();
-
-
-        //ofile << std::setfill(0) << std::setw(2) << timestamp / 1000
-        ofile << "s: Process " << job.PageList[1].BindProcessName << " referenced page "
-              << job.PageList[1].PageNumber << ". Page not in memory. " << endl;
+        ofile.open("Run.txt");
+        ofile << setfill('0') << setw(2) << getCurrentTime() / 1000
+              << "s: Process " << job.pageList[0].bindProcessName << " referenced page "
+              << job.pageList[0].pageNumber << ". Page not in memory. " << endl;
         ofile.close();
 
         if (memoryFreePage.size() > 4) {
-            AssignPage(job, currentReferenceNumber);
-            auto CurrentTime =100;
+            AssignPage(&job, currentReferenceNumber);
+            auto CurrentTime = 100;
             usleep(100);;
         }
-
         auto localTime = CurrentTime; // preserving start time
-        while (CurrentTime<localTime+jobServiceTime && CurrentTime < TotalTime){
-            auto nextReferenceNumber = locality(currentReferenceNumber,job.PageList[1].PageNumber);
-            if (job.PageList[nextReferenceNumber]){
-                job.HitCounts++;
-
-                ofile << setfill('0') << setw(2) << timestamp / 1000
-                      << "s: Process " << curr.name << " referenced page "
-                      << i << ". Page in memory. No page evicted."
+        while (CurrentTime < localTime + job.serviceTime && CurrentTime < TotalTime) {
+            auto nextReferenceNumber = locality(currentReferenceNumber, job.pageList[1].pageNumber);
+            if (job.pageList[nextReferenceNumber].bindProcessName != "") {
+                job.hitCounts++;
+                ofile.open("Run.txt");
+                ofile << setfill('0') << setw(2) << getCurrentTime() / 1000
+                      << "s: Process " << job.name << " referenced page "
+                      << job.hitCounts << ". Page in memory. No page evicted."
                       << endl;
+                ofile.close();
 
-            }else{
-                if (memoryFreePage.size()!=0){
-                    AssignPage(&job,nextReferenceNumber);
-                }else{
-                    RandomRepalce(&job,nextReferenceNumber);
+            } else {
+                if (memoryFreePage.size() != 0) {
+                    AssignPage(&job, nextReferenceNumber);
+                } else {
+                    RandomRepalce(&job, nextReferenceNumber);
                 }
             }
 
-            CurrentTime +=100;
+            CurrentTime += 100;
             usleep(100);
         }
-
-
+        map<int, page>::iterator it;
+        it = job.pageList.begin();
+        while (it != job.pageList.end()) {
+            if (it->first != 0) {
+                job.pageList.erase(it);
+            }
+            it++;
+        }
+        ofile.open("Run.txt");
+        ofile << setfill('0') << setw(2) << getCurrentTime() / 1000
+              << "s: Process " << job.name << " completed. Total size "
+              << job.size << "MB. Duration " << job.serviceTime / 1000 << "s.\n"
+              << "MEMORY MAP. MUST REPLACE!\n";
+        ofile.close();
     }
 }
 
 int main() {
     initVariable();
+    cout<<"Init finish!"<<endl;
     Run();
     return 0;
 }
